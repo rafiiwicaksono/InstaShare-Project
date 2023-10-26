d5a996738ef33b35f1338d433855a52b79bed854
rafiiwicaksono <rafiihsan5@gmail.com>
2023-10-27 02:28:16 +0700

    update all

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const {User, Profile, Post} = require(`../models`)
+const { Op } = require("sequelize");
 class Controller {
     
     static landingPage(req,res){
@@ -17,6 +18,15 @@ class Controller {
             res.send(error.message)
         }
     }
+
+    static async postLogin(req,res){
+        try {
+            console.log(req.body)
+            res.redirect('/post')
+        } catch (error) {
+            res.send(error.message)
+        }
+      }
 
     static signup(req, res) {
         let {errors} = req.query
@@ -49,31 +59,112 @@ class Controller {
             }
         }
     }
+
     static async home(req, res) {
+        let {search} = req.query
+        let conditional = {}
+        if (search) {
+            conditional.username = {
+                [Op.iLike]: `%${search}%`
+            }
+        }
         try {
             let post = await Post.findAll({
                 include: {
-                    model: User,
-                    include: Profile
+                    association: 'User',
+                    where : conditional
                 }
             })
-            res.send(post)
+            res.render('home', {post})
+        } catch (error) {
+            console.log(error)
+            res.send(error.message)
+        }
+    }
+
+    static async addPost(req, res) {
+        try {
+            let user = await User.findAll()
+            res.render('addPost', {user})
         } catch (error) {
             console.log(error.message)
             res.send(error.message)
         }
     }
 
-
-
-      static async postLogin(req,res){
+    static async createPost(req, res) {
         try {
-            console.log(req.body)
-            res.redirect('/post')
+            const {UserId, caption, imgUrl} = req.body
+            await Post.create({UserId, caption, imgUrl})
+            res.redirect(`/home`)
         } catch (error) {
+            console.log(error.message)
             res.send(error.message)
         }
-      }
+    }
+
+    static async likePost(req, res) {
+        try {
+            await Post.increment({likes: 1}, {
+                where: {
+                    id: req.params.id
+                }
+            })
+            res.redirect(`/home`)
+        } catch (error) {
+            console.log(error.message)
+            res.send(error.message)
+        }
+    }
+
+    static async deletePost(req, res) {
+        try {
+            await Post.deletePostMethod(req)
+            res.redirect(`/home`)
+        } catch (error) {
+            console.log(error.message)
+            res.send(error.message)
+        }
+    }
+
+    static async profile(req, res) {
+        try {
+            let profile = await Profile.findAll()
+            res.render('profile', {profile})
+        } catch (error) {
+            console.log(error.message)
+            res.send(error.message)
+        }
+    }
+
+    static async editProfile(req, res) {
+        try {
+            let profile = await Profile.findByPk(req.params.id)
+            res.render('editProfile', {profile})
+        } catch (error) {
+            console.log(error.message)
+            res.send(error.message)
+        }
+    }
+
+    static async updateProfile(req, res) {
+        try {
+            const {firstName, lastName, phone, bio, profilePicture} = req.body
+            await Profile.update({firstName, lastName, phone, bio, profilePicture}, {
+                where: {
+                    id: req.params.id
+                }
+            })
+            res.redirect(`/profile`)
+        } catch (error) {
+            console.log(error)
+            res.send(error.message)
+        }
+    }
+
+
+
+      
 
 }
 
