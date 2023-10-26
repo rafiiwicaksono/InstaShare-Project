62a0efde3e7f0c27ddd7462be441ce56a19b7b7d
rafiiwicaksono <rafiihsan5@gmail.com>
2023-10-26 11:22:35 +0700

    update database

---
+++ migrations/20231026031600-create-profile.js
@@ -0,0 +1,51 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      dateOfBirth: {
+        type: Sequelize.DATE,
+        allowNull:false
+      },
+      gender: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      bio: {
+        type: Sequelize.STRING
+      },
+      profilePicture: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Profiles');
+  }
+};
